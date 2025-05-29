39b0b26874168576a98adcb3e37750a5edf2ff42
Rohit Nayak <rohit77.rn@gmail.com>
2025-05-29 23:20:34 +0530

    initial commit To-do app with dark-mode

--- todo-app/script.js
+++ todo-app/script.js
@@ -4,72 +4,87 @@ const list = document.querySelector("#taskList")
 const toggleTheme = document.getElementById("toggleTheme");
 
 
-window.addEventListener("DOMContentLoaded",() =>{
+window.addEventListener("DOMContentLoaded", () => {
+    const saved = localStorage.getItem('tasks')
+    console.log(saved);
+    if (saved) {
+        task = JSON.parse(saved)
+        displayTask()
+    }
     const stored = localStorage.getItem('theme');
-    if(stored==="dark"){
+    if (stored === "dark") {
         document.body.classList.add('dark-mode')
-        toggleTheme.innerText="Light"
-    } else{
-        toggleTheme.innerText="Dark"
+        toggleTheme.innerHTML = `<i class="fa-solid fa-moon"></i>`
+    } else {
+        toggleTheme.innerHTML = `<i class="fa-solid fa-sun"></i>`
     }
 })
 
 toggleTheme.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-  const isDark = document.body.classList.contains('dark-mode')
-  localStorage.setItem("theme",isDark ? "dark":"light")
+    document.body.classList.toggle("dark-mode");
+    const isDark = document.body.classList.contains('dark-mode')
+    localStorage.setItem("theme", isDark ? "dark" : "light")
 
-  toggleTheme.innerText =isDark ? "Light":"Dark"
+    toggleTheme.innerHTML = isDark ? `<i class="fa-solid fa-moon"></i>` : `<i class="fa-solid fa-sun"></i>`
 });
 
-document.querySelector("#inputText").addEventListener('keydown', (e) =>{
-    if(e.key==='Enter'){
+document.querySelector("#inputText").addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
         addTask()
     }
 })
 
-function addTask(){
-const inputText= document.querySelector("#inputText");
+function addTask() {
+    const inputText = document.querySelector("#inputText");
     const taskText = inputText.value.trim();
     const time = new Date().toLocaleString()
-    if(taskText === "") return
-    task.push({text:taskText,done:false,time})
-    
-    inputText.value=""
+    if (taskText === "") return
+    task.push({ text: taskText, done: false, time })
+
+    inputText.value = ""
+    saveTasks()
     displayTask()
 }
 
 btn.addEventListener('click', addTask)
 
-function deleteTask(index){
-    task.splice(index,1)
+function deleteTask(index) {
+    task.splice(index, 1)
+    saveTasks()
     displayTask()
 }
 
-function toggleTask(index){
+function toggleTask(index) {
     task[index].done = !task[index].done;
+    saveTasks()
     displayTask()
 }
 
-function notask(){
-    list.style.textAlign="center"
+function notask() {
+    list.style.textAlign = "center"
     list.innerHTML = `
     <img src="176171-1.jpg" alt="No Tasks" width="120" style="margin-bottom: 10px; opacity: 0.6;" />
     <div>No tasks yet. Add one!</div>
   `;
 }
 
-function displayTask(){
-    list.innerHTML="";
-    if(task.length === 0){
+function saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(task))
+}
+
+function displayTask() {
+    list.innerHTML = "";
+    if (task.length === 0) {
         notask()
-    } else{
-        task.forEach((task,index) =>{
+    } else {
+        task.forEach((task, index) => {
             const li = document.createElement('li');
             li.classList.add("task")
             li.innerHTML = `
-            <input type="checkbox" ${task.done ? 'checked': ''} onchange="toggleTask(${index})">
-            ${index+1}<span style="text-decoration:${task.done ? 'line-through' : 'none'}"> ${task.text}</span> 
+             <label style="cursor: pointer;">
+                <input type="checkbox" ${task.done ? 'checked' : ''} onchange="toggleTask(${index})">
+                <span style="text-decoration:${task.done ? 'line-through' : 'none'};font-style:${task.done ? 'italic':'none'}"> ${index + 1}. ${task.text}</span>
+            </label>
             <div>
             <span style="color:#a5a6ad;font-size:12px;font-style:italic;margin-right:10px;">Added at : ${task.time}</span>
             <button onclick="deleteTask(${index})">Delete</button>
