a1a944177b896e5a895942935a848453826b62c2
Rohit Nayak <rohit77.rn@gmail.com>
2025-06-11 21:11:53 +0530

    initial commit

---
+++ accoridian/script.js
@@ -0,0 +1,49 @@
+const faqs = [
+    { question: "What is JavaScript?", answer: "JavaScript is used to make websites interactive." },
+    { question: "What is an accordion?", answer: "An accordion lets users expand and collapse sections of content." },
+    { question: "Can I open multiple sections?", answer: "Yes, if your code allows it." },
+    {
+    question: "Can multiple accordion panels be open at once?",
+    answer: "Yes, depending on the logic. You can allow one or multiple panels to stay open based on your JavaScript implementation."
+  },
+  {
+    question: "How can I animate accordion transitions?",
+    answer: "Use CSS transitions with max-height, opacity, or transform properties for smooth expanding/collapsing effects."
+  }
+];
+
+const accordianContainer = document.querySelector('.accordian-container')
+
+faqs.forEach((accordina) => {
+    // accordianContainer.innerHTML = ""
+    accordianContainer.innerHTML += `
+    <div class="accordians">
+                <div class="question">
+                    ${accordina.question}
+                    <i class="icon fa-solid fa-angle-down"></i>
+                </div>
+                <div class="answer">${accordina.answer}
+                </div>
+            </div>
+    `
+})
+
+const accordians = document.querySelectorAll('.accordians');
+
+accordians.forEach((accordian) =>{
+    const icon = accordian.querySelector('.icon')
+    const answer = accordian.querySelector('.answer')
+
+    accordian.addEventListener('click' ,()=>{
+        console.log(icon.classList.contains('active'));
+        
+        if(!icon.classList.contains('active')){
+            icon.classList.add('active')
+            answer.style.maxHeight=answer.scrollHeight + 'px'
+        } else{
+            icon.classList.remove('active')
+            answer.style.maxHeight=null;
+        }
+    })
+})
+
