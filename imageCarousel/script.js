f4cd134178b5bb6b4f3d588da3978a1d6ad4fad3
Rohit Nayak <rohit77.rn@gmail.com>
2025-06-21 16:50:18 +0530

    image-carouselt

--- imageCarousel/script.js
+++ imageCarousel/script.js
@@ -1,22 +1,19 @@
-const carousel = document.querySelector('.carousel')
+const craousel = document.querySelector('.carousel')
 const images = document.querySelectorAll('.carousel img')
-const prevBtn = document.querySelector('.prev-btn')
-const nextBtn = document.querySelector('.next-btn')
+const prevBtn = document.querySelector('.prevBtn')
+const nextBtn = document.querySelector('.nextBtn')
 
 let index = 0;
-
-let ci = setInterval(() => showImages(index + 1), 6000);
-
-function showImages(i) {
-    index = (i + images.length) % images.length
-    carousel.style.transform = `translateX(-${index * 600}px)`
+let ci = setInterval(() => showImages(index + 1),3000)
+function showImages(i){
+    index = (i + images.length) % images.length;
+    // console.log(index);
+    console.log(craousel.style.transform = `translateX(-${index * 700}px)`);
+    
+    craousel.style.transform = `translateX(-${index * 700}px)`
     clearInterval(ci)
-    ci = setInterval(() => showImages(index + 1), 6000);
+    ci = setInterval(() => showImages(index + 1), 3000)
 }
 
-prevBtn.addEventListener('click', () => showImages(index - 1))
-nextBtn.addEventListener('click', () => showImages(index + 1))
-
-
-
-
+prevBtn.addEventListener('click',() => showImages(index - 1 ))
+nextBtn.addEventListener('click',() => showImages(index + 1 ))
